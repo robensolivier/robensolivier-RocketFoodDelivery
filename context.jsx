cab1cbf93f5a93db05f18548457255d87101eb1a
robensolivier <robens.olivier@gmail.com>
2023-05-03 10:39:10 -0400

    final commit

--- context.jsx
+++ context.jsx
@@ -1,3 +1,17 @@
 import React from 'react';
 
 export const AuthContext = React.createContext();
+const signIn = async (data) => {
+    try {
+      // Vérifier si l'utilisateur est déjà connecté
+      const user = await AsyncStorage.getItem('@user');
+      if (user !== null) {
+        navigation.navigate('AccountSelectionScreen');
+        setUser(JSON.parse(user));
+      } else {
+        setAuthState(data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
